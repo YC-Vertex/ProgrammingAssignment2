cb90484ccf317ae387af7790704a9e6091f4e806
qhfz <vertexprivate@gmail.com>
2016-02-04 11:40:37 +0800

    by VertexYC

--- cachematrix.R
+++ cachematrix.R
@@ -1,22 +1,25 @@
 ## this file contains a pair of functions that cache the inverse of a matrix
 
-## function "makeCacheMatrix" creates a special "matrix" object 
-## that can cache its inverse
-
+## function "makeCacheMatrix"
+## creates a special "matrix" object that can cache its inverse
 makeCacheMatrix <- function(newMatrix = matrix()) {
     inverseOfMatrix <- NULL
     
-    set <- function(matrix) {
+    ## function "set" caches the original matrix
+    set <- function(matrix) { 
         newMatrix <<- matrix
         inverseOfMatirx <<- NULL
     }
     
+    ## function "get" returns the original matrix
     get <- function() 
         return(x)
     
+    ## function "setInverse" caches the inverse of the matrix
     setInverse <- function(inverse) 
         inverseOfMatrix <<- inverse
     
+    ## function "getInverse" returns the inverse of the matrix
     getInverse <- function()
         return(inverseOfMatrix)
     
@@ -25,9 +28,8 @@ makeCacheMatrix <- function(newMatrix = matrix()) {
         getInverse = getInverse))
 }
 
-## function "cacheSolve" computes the inverse of the special "matrix"
-## returned by "makeCacheMatrix"
-
+## function "cacheSolve"
+##c omputes the inverse of the special "matrix" returned by "makeCacheMatrix"
 cacheSolve <- function(targetMatrix) {
     result <- targetMatrix[["getInverse"]]()
     
